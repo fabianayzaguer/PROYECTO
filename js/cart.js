0ea16c4566ee90208fdfa796cdc4b7e937d2278a
fabianayzaguer <110476805+fabianayzaguer@users.noreply.github.com>
2022-10-26 16:03:27 -0300

    Entraga 5
    errores en el guardado

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,17 @@
 async function mostrarDatos(){
 
     let usuario = obtenerLocalStorage("usuario")
-let prodID = obtenerLocalStorage("prodID")
-const response = await fetch(`https://japceibal.github.io/emercado-api/products/${prodID}.json`);
+let usuarioID = "25801"
+const response = await fetch(`https://japceibal.github.io/emercado-api/user_cart/25801.json`);
 const json = await response.json();
-let cantidad = 1
 let htmlContentToAppend = "";
 
-console.log(json);
-let htmlparaimagen = ""
-     for (let imagen of json.images){
-        
-        htmlparaimagen += `<img src = "${imagen}"`
-     }
 
-{ htmlContentToAppend += `<div>
+console.log(json);
+
+json.articles.forEach((product) => {
+  
+htmlContentToAppend += `<div>
   <h1>Carrito de compras</h1>
   <h3>Articulos a comprar</h3>
  </div>
@@ -28,16 +25,41 @@ let htmlparaimagen = ""
    <th>Subtotal</th>
  </tr>
  <tr>
-   <th>${htmlparaimagen}}</th>
-   <td>${json.name}</td>
-   <td>${json.cost}${json.currency}</td>
-   <td>${cantidad}</td>
-   <td>${json.cost*cantidad} </td>
+   <th><img src = ${product.image}></th>
+   <td>${product.name}</td>
+   <td>${product.unitCost}${product.currency}</td>
+   <td><input type="number"  id="cantidad"></td>
+   <td id=subtotal>${product.unitCost} </td>
  </div>
 
+<input>
 
- ` };
- document.getElementById("carrito"). innerHTML = htmlContentToAppend;
+ <div>
+ <h4> Tipo de Envío</h4>
+ <input type="radio" value="Premium"
+ <label for="Premium"> Premium: 2 a 5 días (%15)</label><br>
+ <input type="radio" value="Express" 
+ <label for="Express"> Express: 5 a 8 días (%7)</label><br>
+ <input type="radio" value="Standard" 
+ <label for="Standard"> Premium: 12 a 15 días (%5)</label>
+ </div>
+ <div>
+ <h4> Dirección de envío </h4>
+ <a>Calle</a><input type="text"> <a>Número</a><input type="text"><br>
+ <a>Esquina</a><input type="text">
+
+ ` 
+ 
+addEventListener('click', (e) => {});
+
+oninput = (e) => {
+  var cantidad = document.getElementById("cantidad");
+  document.getElementById("subtotal").innerHTML =  product.unitCost*cantidad.value;
+};
+
+} );
+
+document.getElementById("carrito"). innerHTML = htmlContentToAppend;
  document.getElementById("nomusuario"). innerHTML = `${usuario}`;
 
 };
@@ -45,3 +67,6 @@ let htmlparaimagen = ""
 mostrarDatos();
 function obtenerLocalStorage(clave){
     return localStorage.getItem(clave); }
+
+
+
